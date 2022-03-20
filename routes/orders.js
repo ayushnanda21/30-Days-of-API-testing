1e66ba5a973f390f29b3c1efe769054b1218893f
Ayush Nanda <ananda_be18@thapar.edu>
2022-03-20 15:29:47 +0530

    creating orders with referecing, mapping to other models

--- routes/orders.js
+++ routes/orders.js
@@ -6,5 +6,57 @@ const {
     verifyTokenAndAdmin
   } = require("./verifyToken");
 
+//importing order model
+const Order = require("../models/Order");
+const OrderItem = require("../models/Order-Item");
+
+//creating order
+
+router.post("/", async(req,res)=>{
+
+  const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) =>{
+    let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product
+    })
+
+    newOrderItem = await newOrderItem.save();
+
+    return newOrderItem._id;
+}))
+const orderItemsIdsResolved =  await orderItemsIds;
+
+  const newOrder =  new Order({
+      orderItems: orderItemsIdsResolved,
+      shippingAddress1: req.body.shippingAddress1,
+      shippingAddress2: req.body.shippingAddress2,
+      city: req.body.city,
+      zip: req.body.zip,
+      country: req.body.country,
+      phone: req.body.phone,
+      status: req.body.status,
+      totalPrice: req.body.totalPrice,
+      user: req.body.user
+  });
+
+  try{
+      const savedOrder = await newOrder.save();
+      if(!savedOrder){
+          return res.status(400).json("Order can't be created");
+      }
+      res.status(201).json(savedOrder);
+  } catch(err){
+      res.status(500).json({
+          error: err,
+          success: false
+      });
+  }
+
+});
+
+
+
+
+
 
 module.exports = router
